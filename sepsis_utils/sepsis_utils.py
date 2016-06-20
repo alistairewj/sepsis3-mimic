e1870c9bc3b1aad961ad4559a870b58a85eef3c5
Alistair Johnson <alistairewj@gmail.com>
2016-06-20 14:00:45 -0400

    updated cm prints to new syntax

--- sepsis_utils/sepsis_utils.py
+++ sepsis_utils/sepsis_utils.py
@@ -707,11 +707,11 @@ def print_cm(y, yhat):
     FN = cm[1,0]
     TP = cm[1,1]
     N = TN+FP+FN+TP
-    print('   \t{:6s}\t{:6s}').format('yhat=0','yhat=1')
-    print('y=0\t{:6g}\t{:6g}\tNPV={:2.2f}').format(cm[0,0],cm[0,1], 100.0*TN / (TN+FN)) # NPV
-    print('y=1\t{:6g}\t{:6g}\tPPV={:2.2f}').format(cm[1,0],cm[1,1], 100.0*TP / (TP+FP)) # PPV
+    print('   \t{:6s}\t{:6s}'.format('yhat=0','yhat=1'))
+    print('y=0\t{:6g}\t{:6g}\tNPV={:2.2f}'.format(cm[0,0],cm[0,1], 100.0*TN / (TN+FN))) # NPV
+    print('y=1\t{:6g}\t{:6g}\tPPV={:2.2f}'.format(cm[1,0],cm[1,1], 100.0*TP / (TP+FP))) # PPV
     # add sensitivity/specificity as the bottom line
-    print('   \t{:2.2f}\t{:2.2f}\tAcc={:2.2f}').format(100.0*TN/(TN+FP), 100.0*TP/(TP+FN), 100.0*(TP+TN)/N)
+    print('   \t{:2.2f}\t{:2.2f}\tAcc={:2.2f}'.format(100.0*TN/(TN+FP), 100.0*TP/(TP+FN), 100.0*(TP+TN)/N))
     print('   \tSpec\tSens')
 
 def print_op_stats(yhat_all, y_all, yhat_names=None, header=None, idx=None):
