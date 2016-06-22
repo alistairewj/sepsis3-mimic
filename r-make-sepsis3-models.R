1de6720a573f08b4e6d2950d2474d64648646d50
Alistair Johnson <alistairewj@gmail.com>
2016-06-22 09:40:30 -0400

    now allows specifying target name

--- r-make-sepsis3-models.R
+++ r-make-sepsis3-models.R
@@ -9,21 +9,26 @@ if (length(args)==0) {
 # load the data
 sep3 <- read.table(args[1], header=TRUE, sep=",")
 
-# build the model
-formula = "hospital_expire_flag~fp(age) + fp(elixhauser_hospital) + is_male + race_black + race_other"
+# base formula for the model
+formula = "~ fp(age) + fp(elixhauser_hospital) + is_male + race_black + race_other"
+
+# add in the target header, if available
 if (length(args)==3) {
-# we have an additional covariate to add
-formula = paste(formula, args[3], sep=" + ")
+# we also specify the target
+formula = paste(args[3], formula, sep=" ")
+} else {
+formula = paste('hospital_expire_flag', formula, sep=" ")
 }
+# add in additional covariate (4th argument)
+if (length(args)==4) {
+formula = paste(formula, args[4], sep=" + ")
+}
+
 print(formula)
+
+# build the model
 mod.mfp <- mfp(as.formula(formula),data=sep3,family="binomial",verbose=FALSE)
 
 # output a text file of the predictions
 preds <- predict(mod.mfp,type="response")
 write.csv(preds,args[2],row.names=FALSE)
-
-# output a text file which reads:
-# variable name, exponent, coefficient
-#? best way to do this?
-# mod.mfp$formula
-# mod.mfp$coef
