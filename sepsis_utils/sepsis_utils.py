d119e81f1c78d4747616c56fd477838157761568
Alistair Johnson <alistairewj@gmail.com>
2016-06-23 13:33:22 -0400

    streamlined auc table printing functions

--- sepsis_utils/sepsis_utils.py
+++ sepsis_utils/sepsis_utils.py
@@ -196,10 +196,51 @@ def print_demographics(df):
         else:
             print('{:20s}'.format(curr_var))
 
-def print_auc_table(df, preds_header, target_header):
+def calc_predictions(df, preds_header, target_header, model=None):
+    if model is None:
+
+        preds = dict()
+        for x in preds_header:
+            preds[x] = df[x].values
+        return preds
+    elif model == 'baseline':
+        P = len(preds_header)
+        y = df[target_header].values == 1
+        preds = dict()
+        for p in range(P):
+            # build the models and get the predictions
+            model = logit(formula=target_header + " ~ age + elixhauser_hospital" +
+            " + race_black + race_other + is_male + " + preds_header[p],
+            data=df).fit(disp=0)
+
+            # create a list, each element containing the predictions
+            preds[preds_header[p]] = model.predict()
+
+        return preds
+    elif model == 'mfp':
+        # call a subprocess to run the R script to generate fractional polynomial predictions
+        import subprocess
+        # loop through each severity score, build an MFP model for each
+        fn_in = "sepsis3-design-matrix.csv"
+        fn_out = "sepsis3-preds.csv"
+        preds = dict()
+        for p in preds_header:
+            rcmd = ["Rscript r-make-sepsis3-models.R", fn_in, fn_out, target_header, p] # note 4th argument is covariate 'p'
+            err = subprocess.call(' '.join(rcmd), shell=True)
+            if err!=0:
+                print('RScript returned error status {}.'.format(err))
+            else:
+                # load in the predictions
+                pred = pd.read_csv(fn_out, sep=',', header=0)
+                preds[p] = pred.values[:,0]
+        return preds
+    else:
+        print('Unsure what {} means...'.format(model))
+        return None
+
+def print_auc_table(preds, target, preds_header):
     # prints a table of AUROCs and p-values like what was presented in the sepsis 3 paper
-    preds = [df[x].values for x in preds_header]
-    y = df[target_header].values == 1
+    y = target == 1
     P = len(preds)
 
     print('{:5s}'.format(''),end='\t')
@@ -210,111 +251,24 @@ def print_auc_table(df, preds_header, target_header):
     print('')
 
     for p in range(P):
-        print('{:5s}'.format(preds_header[p]),end='\t')
+        ppred = preds_header[p]
+        print('{:5s}'.format(ppred),end='\t')
         for q in range(P):
-            if p==q:
-                auc, ci = ru.bootstrap_auc(preds[p], y, B=100)
-                print('{:0.3f} [{:0.3f}, {:0.3f}]'.format(auc, ci[0], ci[1]), end='\t')
-            elif q>p:
-                #TODO: cronenback alpha
-                print('{:20s}'.format(''),end='\t')
-
-            else:
-                pval, ci = ru.test_auroc(preds[p], preds[q], y)
-                if pval > 0.001:
-                    print('{:0.3f}{:15s}'.format(pval, ''), end='\t')
-                else:
-                    print('< 0.001{:15s}'.format(''),end='\t')
-
-
-        print('')
-
-
-def print_auc_table_baseline(df, preds_header, target_header):
-    # prints a table of AUROCs and p-values
-    # also train the baseline model using df_mdl
-
-    P = len(preds_header)
-    y = df[target_header].values == 1
-
-    print('{:5s}'.format(''),end='\t')
-
-    preds = list()
-    for p in range(P):
-        score_added = preds_header[p]
-        print('{:20s}'.format(preds_header[p]),end='\t')
-
-        # build the models and get the predictions
-        model = logit(formula=target_header + " ~ age + elixhauser_hospital" +
-        " + race_black + race_other + is_male + " + score_added,
-        data=df).fit(disp=0)
-
-        # create a list, each element containing the predictions
-        # we will update this to be model predictions
-        preds.append(model.predict())
-
-    print('')
-
-    for p in range(P):
-        print('{:5s}'.format(preds_header[p]),end='\t')
-
-        for q in range(P):
-            if p==q:
-                auc, ci = ru.bootstrap_auc(preds[p], y, B=100)
-                print('{:0.3f} [{:0.3f}, {:0.3f}]'.format(auc, ci[0], ci[1]), end='\t')
-            elif q>p:
-                #TODO: cronenback alpha
-                print('{:20s}'.format(''),end='\t')
-
-            else:
-                pval, ci = ru.test_auroc(preds[p], preds[q], y)
-                if pval > 0.001:
-                    print('{:0.3f}{:15s}'.format(pval, ''), end='\t')
-                else:
-                    print('< 0.001{:15s}'.format(''),end='\t')
-
-
-        print('')
-
-
-def print_auc_table_given_preds(preds, target, preds_header=None):
-    # prints a table of AUROCs and p-values
-    # also train the baseline model using df_mdl
-    # preds is a dictionary of predictions
-
-    if preds_header is None:
-        preds_header = preds.keys()
-
-    P = len(preds_header)
-    y = target == 1
-
-    print('{:5s}'.format(''),end='\t')
-
-    # print header line
-    for p in range(P):
-        print('{:20s}'.format(preds_header[p]),end='\t')
-    print('')
-
-    for p in range(P):
-        print('{:5s}'.format(preds_header[p]),end='\t')
-        pname = preds_header[p]
-
-        for q in range(P):
-            qname = preds_header[q]
-            if pname not in preds:
+            qpred = preds_header[q]
+            if ppred not in preds:
                 print('{:20s}'.format(''),end='\t') # skip this as we do not have the prediction
             elif p==q:
-                auc, ci = ru.bootstrap_auc(preds[pname], y, B=100)
+                auc, ci = ru.bootstrap_auc(preds[ppred], y, B=100)
                 print('{:0.3f} [{:0.3f}, {:0.3f}]'.format(auc, ci[0], ci[1]), end='\t')
             elif q>p:
                 #TODO: cronenback alpha
                 print('{:20s}'.format(''),end='\t')
 
             else:
-                if qname not in preds:
+                if qpred not in preds:
                     print('{:20s}'.format(''),end='\t') # skip this as we do not have the prediction
                 else:
-                    pval, ci = ru.test_auroc(preds[pname], preds[qname], y)
+                    pval, ci = ru.test_auroc(preds[ppred], preds[qpred], y)
                     if pval > 0.001:
                         print('{:0.3f}{:15s}'.format(pval, ''), end='\t')
                     else:
@@ -323,194 +277,243 @@ def print_auc_table_given_preds(preds, target, preds_header=None):
 
         print('')
 
+def print_auc_table_to_file(preds, target, preds_header=None, filename=None):
+    # prints a table of AUROCs and p-values
+    # also train the baseline model using df_mdl
+    # preds is a dictionary of predictions
+    if filename is None:
+        filename = 'auc_table.csv'
 
+    f = open(filename,'w')
+
+
+    if preds_header is None:
+        preds_header = preds.keys()
+
+    P = len(preds_header)
+    y = target == 1
+    f.write('{}\t'.format(''))
+
+    # print header line
+    for p in range(P):
+        f.write('{}\t'.format(preds_header[p]))
+    f.write('\n')
+
+    for p in range(P):
+        f.write('{}\t'.format(preds_header[p]))
+        pname = preds_header[p]
+        for q in range(P):
+            qname = preds_header[q]
+            if pname not in preds:
+                f.write('{}\t'.format('')) # skip this as we do not have the prediction
+            elif p==q:
+                auc, ci = ru.bootstrap_auc(preds[pname], y, B=100)
+                f.write('{:0.3f} [{:0.3f}, {:0.3f}]\t'.format(auc, ci[0], ci[1]))
+            elif q>p:
+                #TODO: cronenback alpha
+                f.write('{}\t'.format(''))
+
+            else:
+                if qname not in preds:
+                    f.write('{}\t'.format('')) # skip this as we do not have the prediction
+                else:
+                    pval, ci = ru.test_auroc(preds[pname], preds[qname], y)
+                    if pval > 0.001:
+                        f.write('{:0.3f}{}\t'.format(pval, ''))
+                    else:
+                        f.write('< 0.001{}\t'.format(''))
+
+
+        f.write('\n')
+
+    f.close()
 
 def get_physiologic_data(con):
-query = 'SET search_path to ' + schema_name + ';' + \
-"""
-with bg as
-(
-select
-    icustay_id
-    , min(PH) as ArterialPH_Min
-    , max(PH) as ArterialPH_Max
-    , min(PCO2) as PaCO2_Min
-    , max(PCO2) as PaCO2_Max
-    , min(PaO2FiO2) as PaO2FiO2_Min
-    , min(AaDO2) as AaDO2_Min
-from bloodgasfirstdayarterial
-where SPECIMEN_PRED = 'ART'
-group by icustay_id
-)
-, vent as
-(
-select
-    ie.icustay_id
-    , max(case when vd.icustay_id is not null then 1 else 0 end)
-        as MechVent
-from icustays ie
-left join ventdurations vd
-    on ie.icustay_id = vd.icustay_id
-    and vd.starttime <= ie.intime + interval '1' day
-group by ie.icustay_id
-)
-, vaso as
-(
-select
-    ie.icustay_id
-    , max(case when vd.icustay_id is not null then 1 else 0 end)
-        as Vasopressor
-from icustays ie
-left join vasopressordurations vd
-    on ie.icustay_id = vd.icustay_id
-    and vd.starttime <= ie.intime + interval '1' day
-group by ie.icustay_id
+    query = 'SET search_path to ' + schema_name + ';' + \
+    """
+    with bg as
+    (
+    select
+        icustay_id
+        , min(PH) as ArterialPH_Min
+        , max(PH) as ArterialPH_Max
+        , min(PCO2) as PaCO2_Min
+        , max(PCO2) as PaCO2_Max
+        , min(PaO2FiO2) as PaO2FiO2_Min
+        , min(AaDO2) as AaDO2_Min
+    from bloodgasfirstdayarterial
+    where SPECIMEN_PRED = 'ART'
+    group by icustay_id
+    )
+    , vent as
+    (
+    select
+        ie.icustay_id
+        , max(case when vd.icustay_id is not null then 1 else 0 end)
+            as MechVent
+    from icustays ie
+    left join ventdurations vd
+        on ie.icustay_id = vd.icustay_id
+        and vd.starttime <= ie.intime + interval '1' day
+    group by ie.icustay_id
+    )
+    , vaso as
+    (
+    select
+        ie.icustay_id
+        , max(case when vd.icustay_id is not null then 1 else 0 end)
+            as Vasopressor
+    from icustays ie
+    left join vasopressordurations vd
+        on ie.icustay_id = vd.icustay_id
+        and vd.starttime <= ie.intime + interval '1' day
+    group by ie.icustay_id
 
-)
-select
-    ie.icustay_id
-    , vit.HeartRate_Min
-    , vit.HeartRate_Max
-    , vit.SysBP_Min
-    , vit.SysBP_Max
-    , vit.DiasBP_Min
-    , vit.DiasBP_Max
-    , vit.MeanBP_Min
-    , vit.MeanBP_Max
-    , vit.RespRate_Min
-    , vit.RespRate_Max
-    , vit.TempC_Min
-    , vit.TempC_Max
-    , vit.SpO2_Min
-    , vit.SpO2_Max
-
-
-    -- coalesce lab/vital sign glucose
-    , case
-        when vit.Glucose_min < lab.Glucose_Min
-            then vit.Glucose_Min
-        when lab.Glucose_Min < vit.Glucose_Min
-            then lab.Glucose_Min
-        else coalesce(vit.Glucose_Min, lab.Glucose_Min)
-    end as Glucose_Min
-
-    , case
-        when vit.Glucose_Max > 2000 and lab.Glucose_Max > 2000
-            then null
-        when vit.Glucose_Max > 2000
-            then lab.Glucose_Max
-        when lab.Glucose_Max > 2000
-            then vit.Glucose_Max
-        when vit.Glucose_Max > lab.Glucose_Max
-            then vit.Glucose_Max
-        when lab.Glucose_Max > vit.Glucose_Max
-            then lab.Glucose_Max
-        else null
-    end as Glucose_Max
-
-    , gcs.MinGCS as GCS_Min
-
-    -- height in centimetres
-    , case
-        when ht.Height > 100
-         and ht.Height < 250
-             then ht.Height
-        else null
-    end as Height
-
-    -- weight in kgs
-    , case
-        when wt.Weight > 30
-         and wt.Weight < 300
-             then wt.Weight
-        else null
-    end as Height
+    )
+    select
+        ie.icustay_id
+        , vit.HeartRate_Min
+        , vit.HeartRate_Max
+        , vit.SysBP_Min
+        , vit.SysBP_Max
+        , vit.DiasBP_Min
+        , vit.DiasBP_Max
+        , vit.MeanBP_Min
+        , vit.MeanBP_Max
+        , vit.RespRate_Min
+        , vit.RespRate_Max
+        , vit.TempC_Min
+        , vit.TempC_Max
+        , vit.SpO2_Min
+        , vit.SpO2_Max
 
 
-    , lab.ANIONGAP_min
-    , lab.ANIONGAP_max
-    , lab.ALBUMIN_min
-    , lab.ALBUMIN_max
-    , lab.BANDS_min
-    , lab.BANDS_max
-    , lab.BICARBONATE_min
-    , lab.BICARBONATE_max
-    , lab.BILIRUBIN_min
-    , lab.BILIRUBIN_max
-    , lab.CREATININE_min
-    , lab.CREATININE_max
-    , lab.CHLORIDE_min
-    , lab.CHLORIDE_max
+        -- coalesce lab/vital sign glucose
+        , case
+            when vit.Glucose_min < lab.Glucose_Min
+                then vit.Glucose_Min
+            when lab.Glucose_Min < vit.Glucose_Min
+                then lab.Glucose_Min
+            else coalesce(vit.Glucose_Min, lab.Glucose_Min)
+        end as Glucose_Min
 
-    , lab.HEMATOCRIT_min
-    , lab.HEMATOCRIT_max
-    , lab.HEMOGLOBIN_min
-    , lab.HEMOGLOBIN_max
-    , lab.LACTATE_min
-    , lab.LACTATE_max
-    , lab.PLATELET_min
-    , lab.PLATELET_max
-    , lab.POTASSIUM_min
-    , lab.POTASSIUM_max
-    , lab.INR_min
-    , lab.INR_max
+        , case
+            when vit.Glucose_Max > 2000 and lab.Glucose_Max > 2000
+                then null
+            when vit.Glucose_Max > 2000
+                then lab.Glucose_Max
+            when lab.Glucose_Max > 2000
+                then vit.Glucose_Max
+            when vit.Glucose_Max > lab.Glucose_Max
+                then vit.Glucose_Max
+            when lab.Glucose_Max > vit.Glucose_Max
+                then lab.Glucose_Max
+            else null
+        end as Glucose_Max
 
-    --, lab.PTT_min
-    --, lab.PTT_max
-    --, lab.PT_min
-    --, lab.PT_max
+        , gcs.MinGCS as GCS_Min
 
-    , lab.SODIUM_min
-    , lab.SODIUM_max
-    , lab.BUN_min
-    , lab.BUN_max
-    , lab.WBC_min
-    , lab.WBC_max
+        -- height in centimetres
+        , case
+            when ht.Height > 100
+             and ht.Height < 250
+                 then ht.Height
+            else null
+        end as Height
 
-    , rrt.RRT
+        -- weight in kgs
+        , case
+            when wt.Weight > 30
+             and wt.Weight < 300
+                 then wt.Weight
+            else null
+        end as Height
 
-    , case
-        when uo.UrineOutput > 20000
-            then null
-        else uo.UrineOutput
-    end as UrineOutput
 
-    , vent.MechVent
-    , vaso.Vasopressor
+        , lab.ANIONGAP_min
+        , lab.ANIONGAP_max
+        , lab.ALBUMIN_min
+        , lab.ALBUMIN_max
+        , lab.BANDS_min
+        , lab.BANDS_max
+        , lab.BICARBONATE_min
+        , lab.BICARBONATE_max
+        , lab.BILIRUBIN_min
+        , lab.BILIRUBIN_max
+        , lab.CREATININE_min
+        , lab.CREATININE_max
+        , lab.CHLORIDE_min
+        , lab.CHLORIDE_max
 
-    , bg.AADO2_min
-    , case
-        when bg.PaO2FiO2_min > 1000
-            then null
-        else bg.PaO2FiO2_min
-    end as PaO2FiO2_min
-    , bg.ArterialPH_min
-    , bg.ArterialPH_max
-    , bg.PaCO2_min
-    , bg.PaCO2_max
+        , lab.HEMATOCRIT_min
+        , lab.HEMATOCRIT_max
+        , lab.HEMOGLOBIN_min
+        , lab.HEMOGLOBIN_max
+        , lab.LACTATE_min
+        , lab.LACTATE_max
+        , lab.PLATELET_min
+        , lab.PLATELET_max
+        , lab.POTASSIUM_min
+        , lab.POTASSIUM_max
+        , lab.INR_min
+        , lab.INR_max
 
-from icustays ie
-left join vitalsfirstday vit
-    on ie.icustay_id = vit.icustay_id
-left join gcsfirstday gcs
-    on ie.icustay_id = gcs.icustay_id
-left join heightfirstday ht
-    on ie.icustay_id = ht.icustay_id
-left join weightfirstday wt
-    on ie.icustay_id = wt.icustay_id
-left join labsfirstday lab
-    on ie.icustay_id = lab.icustay_id
-left join rrtfirstday rrt
-    on ie.icustay_id = rrt.icustay_id
-left join uofirstday uo
-    on ie.icustay_id = uo.icustay_id
-left join vent
-    on ie.icustay_id = vent.icustay_id
-left join vaso
-    on ie.icustay_id = vaso.icustay_id
-left join bg
-    on ie.icustay_id = bg.icustay_id
-"""
+        --, lab.PTT_min
+        --, lab.PTT_max
+        --, lab.PT_min
+        --, lab.PT_max
 
-dd = pd.read_sql_query(query,con)
-return dd
+        , lab.SODIUM_min
+        , lab.SODIUM_max
+        , lab.BUN_min
+        , lab.BUN_max
+        , lab.WBC_min
+        , lab.WBC_max
+
+        , rrt.RRT
+
+        , case
+            when uo.UrineOutput > 20000
+                then null
+            else uo.UrineOutput
+        end as UrineOutput
+
+        , vent.MechVent
+        , vaso.Vasopressor
+
+        , bg.AADO2_min
+        , case
+            when bg.PaO2FiO2_min > 1000
+                then null
+            else bg.PaO2FiO2_min
+        end as PaO2FiO2_min
+        , bg.ArterialPH_min
+        , bg.ArterialPH_max
+        , bg.PaCO2_min
+        , bg.PaCO2_max
+
+    from icustays ie
+    left join vitalsfirstday vit
+        on ie.icustay_id = vit.icustay_id
+    left join gcsfirstday gcs
+        on ie.icustay_id = gcs.icustay_id
+    left join heightfirstday ht
+        on ie.icustay_id = ht.icustay_id
+    left join weightfirstday wt
+        on ie.icustay_id = wt.icustay_id
+    left join labsfirstday lab
+        on ie.icustay_id = lab.icustay_id
+    left join rrtfirstday rrt
+        on ie.icustay_id = rrt.icustay_id
+    left join uofirstday uo
+        on ie.icustay_id = uo.icustay_id
+    left join vent
+        on ie.icustay_id = vent.icustay_id
+    left join vaso
+        on ie.icustay_id = vaso.icustay_id
+    left join bg
+        on ie.icustay_id = bg.icustay_id
+    """
+
+    dd = pd.read_sql_query(query,con)
+    return dd
