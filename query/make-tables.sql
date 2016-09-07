bd94f5c06c3bbf887c8fec879e326735f5738c5d
Alistair Johnson <alistairewj@gmail.com>
2016-09-07 15:47:22 -0400

    add severity score dependencies

--- query/make-tables.sql
+++ query/make-tables.sql
@@ -10,15 +10,26 @@ BEGIN;
 
 \i tbls/suspicion-of-infection.sql
 
--- Generate the views which the severity scores are based on
+-- Generate the views which the severity scores are based on (at time of infection)
 \i tbls/urine-output-infect-time.sql
 \i tbls/vitals-infect-time.sql
 \i tbls/gcs-infect-time.sql
 \i tbls/labs-infect-time.sql
 \i tbls/blood-gas-infect-time.sql
 \i tbls/blood-gas-arterial-infect-time.sql
-
 \i tbls/vaso-dur.sql
+
+-- Generate the views which the severity scores are based on (first 24 hours)
+\i ../mimic-code/etc/firstday/urine-output-first-day.sql
+\i ../mimic-code/etc/firstday/ventilation-first-day.sql
+\i ../mimic-code/etc/firstday/vitals-first-day.sql
+\i ../mimic-code/etc/firstday/gcs-first-day.sql
+\i ../mimic-code/etc/firstday/labs-first-day.sql
+\i ../mimic-code/etc/firstday/blood-gas-first-day.sql
+\i ../mimic-code/etc/firstday/blood-gas-first-day-arterial.sql
+
+\i ../mimic-code/etc/ventilation-durations.sql
+\i ../mimic-code/etc/echo-data.sql
 \i ../mimic-code/etc/firstday/weight-first-day.sql
 \i ../mimic-code/etc/firstday/height-first-day.sql
 \i ../mimic-code/comorbidity/postgres/elixhauser-ahrq-v37-with-drg.sql
