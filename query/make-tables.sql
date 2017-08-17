44dc013e517292d8ac5b8edc0a239d77dc0332c2
Alistair Johnson <alistairewj@gmail.com>
2017-08-17 17:12:31 -0400

    remove extra tables, calculate some directly from mimic-code

--- query/make-tables.sql
+++ query/make-tables.sql
@@ -8,16 +8,26 @@ BEGIN;
 -- ---------- STAGE 1 ---------- --
 -- ----------------------------- --
 
+\i tbls/abx-poe-list.sql
+\i tbls/abx-micro-prescription.sql
 \i tbls/suspicion-of-infection.sql
 
--- Generate the views which the severity scores are based on (at time of infection)
-\i tbls/urine-output-infect-time.sql
-\i tbls/vitals-infect-time.sql
-\i tbls/gcs-infect-time.sql
-\i tbls/labs-infect-time.sql
-\i tbls/blood-gas-infect-time.sql
-\i tbls/blood-gas-arterial-infect-time.sql
-\i tbls/vaso-dur.sql
+-- generate cohort
+\i tbls/cohort.sql
+
+-- generate sepsis definitions
+\i ../mimic-code/concepts/sepsis/angus.sql
+\i ../mimic-code/concepts/sepsis/martin.sql
+\i ../mimic-code/concepts/sepsis/explicit.sql
+
+-- -- Generate the views which the severity scores are based on (at time of infection)
+-- \i tbls/urine-output-infect-time.sql
+-- \i tbls/vitals-infect-time.sql
+-- \i tbls/gcs-infect-time.sql
+-- \i tbls/labs-infect-time.sql
+-- \i tbls/blood-gas-infect-time.sql
+-- \i tbls/blood-gas-arterial-infect-time.sql
+-- \i tbls/vaso-dur.sql
 
 -- Generate the views which the severity scores are based on (first 24 hours)
 \i ../mimic-code/etc/firstday/urine-output-first-day.sql
@@ -46,17 +56,17 @@ BEGIN;
 \i ../mimic-code/severityscores/qsofa.sql
 \i ../mimic-code/severityscores/sofa.sql
 
--- Severity scores at the time of suspected infection
-\i tbls/sofa-si.sql
-\i tbls/sirs-si.sql
-\i tbls/lods-si.sql
-\i tbls/qsofa-si.sql
-\i tbls/mlods-si.sql
+-- -- Severity scores at the time of suspected infection
+-- \i tbls/sofa-si.sql
+-- \i tbls/sirs-si.sql
+-- \i tbls/lods-si.sql
+-- \i tbls/qsofa-si.sql
+-- \i tbls/mlods-si.sql
 
--- Severity scores on admission
-\i tbls/qsofa-admission.sql
-\i tbls/blood-gas-admission.sql
-\i tbls/sirs-admission.sql
+-- -- Severity scores on admission
+-- \i tbls/qsofa-admission.sql
+-- \i tbls/blood-gas-admission.sql
+-- \i tbls/sirs-admission.sql
 
 -- Generate the final table
 \i tbls/sepsis3.sql
